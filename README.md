d9ef62896aa6e3e4b6e57be6d0f559421020bc3f
Yuji Shimojima <yuji.shimojima.s@gmail.com>
2024-04-11 05:03:03 +0200

    first commit to run comand is nix develop flake.nix

---
+++ README.md
@@ -0,0 +1,1 @@
+# nixDevEnv
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "base-tool-nixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; # 特定のバージョンを指定
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true; # ROCmなどの非フリーパッケージを利用する場合に必要
+          };
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.gcc13
+            pkgs.curl
+            pkgs.python313
+            pkgs.texlive.combined.scheme-full # TexLiveのフルインストール
+            # ここにROCmのパッケージを追加
+            pkgs.rocmPackages.rpp
+            pkgs.rocmPackages.clr
+            pkgs.rocmPackages.hipcc
+            pkgs.rocmPackages.rocm-smi
+            # 必要に応じて他のROCm関連パッケージを追加
+            pkgs.gnuplot
+            pkgs.gnumake
+            pkgs.unzip
+          ];
+        };
+      }
+    );
+}
